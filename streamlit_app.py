57dc18f50487fa695acc91a5663237ad8c921a6c
Satheesh Vivekanandan <114069477+Satheeshvivek@users.noreply.github.com>
2022-09-21 14:44:58 +0400

    Update streamlit_app.py
    Changed Title, Added New Header and Emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
-streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('kale, spinach& Rocket smoothie')
-streamlit.text('Hard-Boiled Free - Range Egg')
+streamlit.title('My Mom's New Healthy Diner')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 kale, spinach& Rocket smoothie')
+streamlit.text('🐔Hard-Boiled Free - Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
+streamlit.header('🍊🍋Build Your Own🍇🍈  Fruit Smoothie 🍍🍉')
